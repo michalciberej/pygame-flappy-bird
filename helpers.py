a8d72c5172b8457961c2a73e905bc9a701157e92
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-21 22:47:06 +0200

    Added spawn_pipes

--- helpers.py
+++ helpers.py
@@ -1,7 +1,23 @@
+import random
 import pygame
+from classes import Pipe
+from assets import TOP_PIPE_IMAGE, BOTTOM_PIPE_IMAGE
+
 
 def quit_game():
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
+
+
+def spawn_pipes(pipes_group, pipes_spawn_timer):
+  if pipes_spawn_timer <= 0:
+    x = 550
+    top_y = random.randint(-700, -400)
+    bottom_y = top_y + random.randint(80, 130) + BOTTOM_PIPE_IMAGE.get_height()
+    pipes_group.add(Pipe(x, top_y, TOP_PIPE_IMAGE, "top"))
+    pipes_group.add(Pipe(x, bottom_y, BOTTOM_PIPE_IMAGE, "bottom"))
+    pipes_spawn_timer = random.randint(150, 250)
+  pipes_spawn_timer -= 1
+  return pipes_spawn_timer
