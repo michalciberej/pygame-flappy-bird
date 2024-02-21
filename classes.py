9b9cb96296c83e681fbfea48f37536bb9a694ca5
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-21 22:46:04 +0200

    Extended Sprite with Pipe class

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 import pygame
 from assets import GROUND_IMAGE, BIRD_IMAGES
-from variables import scroll_speed, WIN_W, BIRD_START_POS_X, BIRD_START_POS_Y, BOTTOM_BORDER, TOP_BORDER
+from variables import scroll_speed, WIN_W, BIRD_START_POS_X, BIRD_START_POS_Y, BOTTOM_BORDER, TOP_BORDER, score
 
 class Ground(pygame.sprite.Sprite):
   def __init__(self, x, y):
@@ -26,7 +26,7 @@ class Bird(pygame.sprite.Sprite):
     self.velocity = 0
     self.fly = False
     self.alive = True
-  
+
   def update(self, user_input):
     self.image_index += 1
     if (self.image_index >= 30):
@@ -49,3 +49,36 @@ class Bird(pygame.sprite.Sprite):
       self.velocity = -6
     
     self.image = pygame.transform.rotate(self.image, self.velocity * -6)
+
+
+class Pipe(pygame.sprite.Sprite):
+  def __init__(self, x, y, image, type):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.type = type
+    self.entered = False
+    self.exited = False
+    self.passed = False
+
+  def update(self):
+    global score
+
+    self.rect.x -= scroll_speed
+    if self.rect.x <= -WIN_W:
+      self.kill()
+
+    if self.type == "bottom":
+      if BIRD_START_POS_X > self.rect.topleft[0] and not self.passed:
+        self.entered = True
+        print("entered " + str(self.entered))
+      if BIRD_START_POS_X > self.rect.topright[0] and not self.passed:
+        self.exited = True
+        print("exited " + str(self.exited))
+      if self.entered and self.exited and not self.passed:
+        self.passed = True
+        print("passed " + str(self.passed))
+        score += 1
+        print(score)
