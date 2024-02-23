578ab605912069c13235450435d242fd368731d3
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-23 20:22:05 +0200

    Added colision  with coins

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
 import random
 import pygame
-from classes import Pipe, Bird
+from classes import Pipe, Coin
 from assets import TOP_PIPE_IMAGE, BOTTOM_PIPE_IMAGE
+from variables import WIN_H, WIN_W, COIN_SIZE
 
 
 def quit_game():
@@ -11,20 +12,30 @@ def quit_game():
       exit()
 
 
-def spawn_pipes(pipes_group, pipes_spawn_timer):
+def spawn_pipes(pipes_group, pipes_spawn_timer, coins):
   if pipes_spawn_timer <= 0:
     x = 550
+    gap = random.randint(80, 130)
     top_y = random.randint(-700, -400)
-    bottom_y = top_y + random.randint(80, 130) + BOTTOM_PIPE_IMAGE.get_height()
+    bottom_y = top_y + gap + BOTTOM_PIPE_IMAGE.get_height()
     pipes_group.add(Pipe(x, top_y, TOP_PIPE_IMAGE, "top"))
     pipes_group.add(Pipe(x, bottom_y, BOTTOM_PIPE_IMAGE, "bottom"))
+    coins.add(Coin((x + (TOP_PIPE_IMAGE.get_width() // 2)) - COIN_SIZE[0] // 2, top_y + TOP_PIPE_IMAGE.get_height() + gap // 2 - COIN_SIZE[1] // 2 ))
     pipes_spawn_timer = random.randint(150, 250)
   pipes_spawn_timer -= 1
   return pipes_spawn_timer
 
 
-def check_colisions(bird, pipes, ground):
+def check_colisions(bird, pipes, ground, coins):
     colision_pipes = pygame.sprite.spritecollide(bird.sprites()[0], pipes, False)
     colision_ground = pygame.sprite.spritecollide(bird.sprites()[0], ground, False)
+    colision_coin = pygame.sprite.spritecollide(bird.sprites()[0], coins, False)
     if colision_ground or colision_pipes:
       bird.sprite.alive = False
+    if colision_coin:
+      coins.sprites()[0].kill()
+      bird.sprite.coins += 1
+
+
+def place_in_middle(image):
+  return ((WIN_W / 2 - image.get_width() / 2), (WIN_H / 2 - image.get_height() / 2))
