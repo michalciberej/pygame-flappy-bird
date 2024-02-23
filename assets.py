21bb311d3045a8b262067b512a1cbb25fb83638f
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-23 20:19:57 +0200

    Added coin assets

--- assets.py
+++ assets.py
@@ -7,6 +7,13 @@ TOP_PIPE_IMAGE = pygame.image.load("assets/pipe_top.png")
 BOTTOM_PIPE_IMAGE = pygame.image.load("assets/pipe_bottom.png")
 GAMEOVER_IMAGE = pygame.image.load("assets/game_over.png")
 GAMESTART_IMAGE = pygame.image.load("assets/start.png")
+COIN_IMAGES = [
+    pygame.image.load("assets/coin.png"),
+    pygame.image.load("assets/coin2.png"),
+    pygame.image.load("assets/coin3.png"),
+    pygame.image.load("assets/coin4.png"),
+    pygame.image.load("assets/coin5.png")
+    ]
 BIRD_IMAGES = [
     pygame.image.load("assets/bird_down.png"),
     pygame.image.load("assets/bird_mid.png"),
