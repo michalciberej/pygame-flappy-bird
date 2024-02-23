8056cece3e5d86b8033e5e2e7a38feb3243a1d76
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-23 20:21:31 +0200

    Added Coin class and updated score counter

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 import pygame
-from assets import GROUND_IMAGE, BIRD_IMAGES
-from variables import scroll_speed, WIN_W, BIRD_START_POS_X, BIRD_START_POS_Y, BOTTOM_BORDER, TOP_BORDER, score
+from assets import GROUND_IMAGE, BIRD_IMAGES, COIN_IMAGES
+from variables import WIN_W, BIRD_START_POS_X, BIRD_START_POS_Y, BOTTOM_BORDER, TOP_BORDER, COIN_SIZE, SCROLL_SPEED
 
 class Ground(pygame.sprite.Sprite):
   def __init__(self, x, y):
@@ -11,7 +11,7 @@ class Ground(pygame.sprite.Sprite):
     self.rect.y = y
   
   def update(self):
-    self.rect.x -= scroll_speed
+    self.rect.x -= SCROLL_SPEED
     if self.rect.x <= -WIN_W:
       self.kill()
 
@@ -26,6 +26,8 @@ class Bird(pygame.sprite.Sprite):
     self.velocity = 0
     self.fly = False
     self.alive = True
+    self.coins = 0
+    self.score = 0
 
   def update(self, user_input):
     self.image_index += 1
@@ -64,23 +66,44 @@ class Pipe(pygame.sprite.Sprite):
     self.entered = False
     self.exited = False
     self.passed = False
+    self.counted = False
 
   def update(self):
-    global score
-
-    self.rect.x -= scroll_speed
+    self.rect.x -= SCROLL_SPEED
     if self.rect.x <= -WIN_W:
       self.kill()
 
     if self.type == "bottom":
       if BIRD_START_POS_X > self.rect.topleft[0] and not self.passed:
         self.entered = True
-        print("entered " + str(self.entered))
       if BIRD_START_POS_X > self.rect.topright[0] and not self.passed:
         self.exited = True
-        print("exited " + str(self.exited))
       if self.entered and self.exited and not self.passed:
         self.passed = True
-        print("passed " + str(self.passed))
-        score += 1
-        print(score)
+    
+  def count(self):
+    if not self.counted and self.passed and self.type == "bottom":
+      self.counted = True
+      return self.counted
+  
+
+
+class Coin(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.transform.smoothscale(COIN_IMAGES[0], COIN_SIZE)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.image_index = 0
+  
+  def update(self):
+    self.image_index += 1
+    
+    self.rect.x -= SCROLL_SPEED
+    if self.rect.x <= -WIN_W:
+      self.kill()
+    
+    if (self.image_index >= 50):
+      self.image_index = 0
+    self.image = pygame.transform.smoothscale(COIN_IMAGES[self.image_index // 10], COIN_SIZE)
