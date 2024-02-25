9e4bfb58faa2461136254b9f4b4b77997a1cc86e
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-25 22:22:10 +0200

    Added dist folder with .exe file

---
+++ dist/assets.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+BACKGROUND_IMAGE = pygame.image.load("assets/background.png")
+GROUND_IMAGE = pygame.image.load("assets/ground.png")
+TOP_PIPE_IMAGE = pygame.image.load("assets/pipe_top.png")
+BOTTOM_PIPE_IMAGE = pygame.image.load("assets/pipe_bottom.png")
+GAMEOVER_IMAGE = pygame.image.load("assets/game_over.png")
+GAMESTART_IMAGE = pygame.image.load("assets/start.png")
+COIN_IMAGES = [
+    pygame.image.load("assets/coin.png"),
+    pygame.image.load("assets/coin2.png"),
+    pygame.image.load("assets/coin3.png"),
+    pygame.image.load("assets/coin4.png"),
+    pygame.image.load("assets/coin5.png")
+    ]
+BIRD_IMAGES = [
+    pygame.image.load("assets/bird_down.png"),
+    pygame.image.load("assets/bird_mid.png"),
+    pygame.image.load("assets/bird_up.png")
+    ]
---
+++ dist/classes.py
@@ -0,0 +1,111 @@
+import pygame
+from data.assets import GROUND_IMAGE, BIRD_IMAGES, COIN_IMAGES
+from data.variables import WIN_W, BIRD_START_POS_X, BIRD_START_POS_Y, BOTTOM_BORDER, TOP_BORDER, COIN_SIZE, SCROLL_SPEED
+
+class Ground(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = GROUND_IMAGE
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+  
+  def update(self):
+    self.rect.x -= SCROLL_SPEED
+    if self.rect.x <= -WIN_W:
+      self.kill()
+
+
+class Bird(pygame.sprite.Sprite):
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = BIRD_IMAGES[0]
+    self.rect = self.image.get_rect()
+    self.rect.center = (BIRD_START_POS_X, BIRD_START_POS_Y)
+    self.image_index = 0
+    self.velocity = 0
+    self.fly = False
+    self.alive = True
+    self.coins = 0
+    self.score = 0
+    self.immunity = False
+
+  def update(self, user_input):
+    self.image_index += 1
+    if (self.image_index >= 30):
+      self.image_index = 0
+    self.image = BIRD_IMAGES[self.image_index // 10]
+
+    self.velocity += 0.5
+    
+    if self.velocity > 6:
+      self.velocity = 6
+    if self.rect.y < BOTTOM_BORDER:
+      self.rect.y += int(self.velocity)
+    if self.rect.y >= BOTTOM_BORDER:
+      self.alive = False
+    if self.velocity == 0:
+      self.fly = False
+    if not self.alive and not self.immunity:
+      self.image = BIRD_IMAGES[0]
+    
+    if user_input[pygame.K_SPACE] and self.rect.y > TOP_BORDER and not self.fly and self.alive:
+      self.fly = True
+      self.velocity = -6
+    
+    self.image = pygame.transform.rotate(self.image, self.velocity * -6)
+
+
+
+class Pipe(pygame.sprite.Sprite):
+  def __init__(self, x, y, image, type):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.type = type
+    self.entered = False
+    self.exited = False
+    self.passed = False
+    self.counted = False
+
+  def update(self):
+    self.rect.x -= SCROLL_SPEED
+    if self.rect.x <= -WIN_W:
+      self.kill()
+
+    if self.type == "bottom":
+      if BIRD_START_POS_X > self.rect.topleft[0] and not self.passed:
+        self.entered = True
+      if BIRD_START_POS_X > self.rect.topright[0] and not self.passed:
+        self.exited = True
+      if self.entered and self.exited and not self.passed:
+        self.passed = True
+    
+  def count(self):
+    if not self.counted and self.passed and self.type == "bottom":
+      self.counted = True
+      return self.counted
+  
+
+
+class Coin(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.transform.smoothscale(COIN_IMAGES[0], COIN_SIZE)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.image_index = 0
+  
+  def update(self):
+    self.image_index += 1
+    
+    self.rect.x -= SCROLL_SPEED
+    if self.rect.x <= -WIN_W:
+      self.kill()
+    
+    if (self.image_index >= 50):
+      self.image_index = 0
+    self.image = pygame.transform.smoothscale(COIN_IMAGES[self.image_index // 10], COIN_SIZE)
